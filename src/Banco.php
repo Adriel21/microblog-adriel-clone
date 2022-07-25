5e66af35b2b70ec86d035fe93a0916e93485e5fe
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-07-25 09:25:14 -0300

    Conexão com banco de dados

--- src/Banco.php
+++ src/Banco.php
@@ -1,30 +1,40 @@
 <?php
 namespace Microblog;
-use PDO, Exception;
+
+/* Indicamos o uso das classes nativas do PHP (ou seja, classes que não fazem parte do nosso namespace) */
+use PDO;
+use Exception;
+
 
 abstract class Banco {
+    /* Propriedades/Atributos de acesso ao servidor de Banco de dados */
     private static string $servidor = "localhost";
     private static string $usuario = "root";
     private static string $senha = "";
     private static string $banco = "microblog_tiago";
-    private static PDO $conexao; 
+    /* private static \PDO $conexao; não precisa do use PDO */
+    private static PDO $conexao; //precisa do use PDO
 
+    /* Método de conexão ao banco */
     public static function conecta():PDO {
         try {
             self::$conexao = new PDO(
-                "mysql:host=".self::$servidor."; 
-                dbname=".self::$banco.";
-                charset=utf8",
-                self::$usuario, 
-                self::$senha
+                "mysql:host=".self::$servidor.";dbname=".self::$banco.";charset=utf8",
+                self::$usuario,
+                self::$senha //self:: acessa os atributos de uma classe static 
             );
-            self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            // Habilita a verificação de erros no código
+            self::$conexao->setAttribute(
+                PDO::ATTR_ERRMODE, //constante de erros em geral
+                PDO::ERRMODE_EXCEPTION // constante de exceções de erro
+            );
 
-            //echo "Ok!"; // teste
         } catch (Exception $erro) {
             die("Deu ruim: ".$erro->getMessage());
         }
         return self::$conexao;
     }
+
 }
-//Banco::conecta(); // teste
+// Banco::conecta();
---
+++ src/Usuario.php
@@ -0,0 +1,140 @@
+<?php
+namespace Microblog;
+use PDO, Exception;
+
+final class Usuario {
+    private int $id;
+    private string $nome;
+    private string $email;
+    private string $senha;
+    private string $tipo;
+    private PDO $conexao;
+
+    public function __construct()
+    {
+        $this->conexao = Banco::conecta();
+    }
+
+    public function listar():array {
+        $sql = "SELECT id, nome, email, tipo FROM usuarios ORDER BY nome";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->execute();
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro: ".$erro->getMessage());
+        }
+        return $resultado;
+    }
+
+   
+
+
+
+    public function inserir():void {
+        $sql = "INSERT INTO usuarios(nome, email, senha, tipo)
+        VALUES(:nome, :email, :senha, :tipo)";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindParam(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->bindParam(":email", $this->email, PDO::PARAM_STR);
+            $consulta->bindParam(":senha", $this->senha, PDO::PARAM_STR);
+            $consulta->bindParam(":tipo", $this->tipo, PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro: ".$erro->getMessage());
+        } 
+    }
+
+    public function codificaSenha(string $senha):string {
+        return password_hash($senha, PASSWORD_DEFAULT);
+    }
+
+    //getters e setters
+    
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+
+    public function setId(int $id)
+    {
+        $this->id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+        return $this;
+    }
+
+
+
+
+   
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+
+
+    public function setNome(string $nome)
+    {
+        $this->nome = filter_var($nome, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+
+ 
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+  
+
+    public function setEmail(string $email)
+    {
+        $this->email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+        return $this;
+    }
+
+  
+
+    public function getSenha(): string
+    {
+        return $this->senha;
+    }
+
+   
+
+    public function setSenha(string $senha)
+    {
+        $this->senha = filter_var($senha, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+
+   
+    public function getTipo(): string
+    {
+        return $this->tipo;
+    }
+
+ 
+    public function setTipo(string $tipo)
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+}
+/* try {
+    $consulta = $this->conexao->prepare($sql);
+    $conusulta->execute();
+} catch (Exception $erro) {
+    die("Erro: ".$erro->getMessage());
+} */
